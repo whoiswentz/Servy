97bbe8ca5984a1d1ed75ba75d3e0c965bdda078c
Vinicios Wentz <vinicios@wentz.io>
2021-09-01 22:13:29 -0300

    feat: playing with messages

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -4,6 +4,7 @@ defmodule Servy.Handler do
   alias Servy.Request
   alias Servy.BearController
   alias Servy.Api
+  alias Servy.VideoCam
 
   import Servy.Parser, only: [parse: 1]
   import Servy.Plugins, only: [rewrite_path: 1, log: 1, track: 1]
@@ -18,6 +19,33 @@ defmodule Servy.Handler do
     |> route
     |> track
     |> format_response
+  end
+
+  def route(%Request{method: "GET", path: "/snapshots"} = request) do
+    parent = self()
+
+    spawn(fn -> send(parent, {:result, VideoCam.get_snapshot("camera-1")}) end)
+    spawn(fn -> send(parent, {:result, VideoCam.get_snapshot("camera-2")}) end)
+    spawn(fn -> send(parent, {:result, VideoCam.get_snapshot("camera-3")}) end)
+
+    snapshot1 =
+      receive do
+        {:result, filename} -> filename
+      end
+
+    snapshot2 =
+      receive do
+        {:result, filename} -> filename
+      end
+
+    snapshot3 =
+      receive do
+        {:result, filename} -> filename
+      end
+
+    snapshots = [snapshot1, snapshot2, snapshot3]
+
+    %{request | status_code: 200, response_body: inspect(snapshots)}
   end
 
   def route(%Request{method: "GET", path: "/wildthings"} = request) do
