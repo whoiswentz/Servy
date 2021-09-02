593df15493592312b8154b21d3f5acde18b933b8
Vinicios Wentz <wentz.vinicios@gmail.com>
2021-09-02 15:06:58 -0300

    feat: typespecs

--- lib/servy/model/bear.ex
+++ lib/servy/model/bear.ex
@@ -1,10 +1,15 @@
 defmodule Servy.Bear do
-  defstruct id: nil,
-            name: "",
-            type: "",
-            hibernating: false
+  alias Servy.Bear
 
-  def order_asc_by_name(a, b) do
-    a.name <= b.name
-  end
+  @type id :: Integer.t()
+  @type name :: String.t()
+  @type type :: String.t()
+  @type hibernating :: Boolean.t()
+  @type t :: %Servy.Bear{id: id, name: name, type: type, hibernating: hibernating}
+
+  @enforce_keys [:id, :name, :type, :hibernating]
+  defstruct [:id, :name, :type, :hibernating]
+
+  @spec order_asc_by_name(Bear.t(), Bear.t()) :: boolean
+  def order_asc_by_name(a, b), do: a.name <= b.name
 end
