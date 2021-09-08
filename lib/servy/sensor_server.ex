8bfd9eb59b17a9e09eb277078a2bbe89449b1b73
Vinicios Wentz <vinicios@wentz.io>
2021-09-08 20:47:37 -0300

    feat: shedule backgroud job to refresh sensor server state

---
+++ lib/servy/sensor_server.ex
@@ -0,0 +1,48 @@
+defmodule Servy.SensorServer do
+  use GenServer
+
+  require Logger
+
+  alias Servy.VideoCam
+  alias Servy.Tracker
+
+  @name __MODULE__
+
+  def start do
+    GenServer.start(__MODULE__, %{}, name: @name)
+  end
+
+  def get_sensors_data do
+    GenServer.call(@name, :get_sensors_data)
+  end
+
+  def run_tasks_to_get_sensor_data do
+    task = Task.async(Tracker, :get_location, ["bigfoot"])
+
+    snapshots =
+      ["camera-1", "camera-2", "camera-3"]
+      |> Enum.map(&Task.async(VideoCam, :get_snapshot, [&1]))
+      |> Enum.map(&Task.await/1)
+
+    where_is_bigfoot = Task.await(task)
+
+    %{snapshots: snapshots, where_is_bigfoot: where_is_bigfoot}
+  end
+
+  def init(_init_state) do
+    init_state = run_tasks_to_get_sensor_data()
+    Process.send_after(self(), :refresh, :timer.seconds(5))
+    {:ok, init_state}
+  end
+
+  def handle_call(:get_sensors_data, _from, state) do
+    {:reply, state, state}
+  end
+
+  def handle_info(:refresh, _state) do
+    Logger.info("refreshing the sensor data")
+    new_state = run_tasks_to_get_sensor_data()
+    Process.send_after(self(), :refresh, :timer.seconds(5))
+    {:noreply, new_state}
+  end
+end
--- lib/servy/video_cam.ex
+++ lib/servy/video_cam.ex
@@ -2,6 +2,6 @@ defmodule Servy.VideoCam do
   def get_snapshot(camera_name) do
     :timer.sleep(1_000)
 
-    "#{camera_name}-snapshot.jpg"
+    "#{camera_name}-snapshot-#{:rand.uniform(10000)}.jpg"
   end
 end
