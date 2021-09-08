510e93e23db42dcddad8a336ac257f9235917f3e
Vinicios Wentz <vinicios@wentz.io>
2021-09-08 20:15:36 -0300

    feat: change implementation of GenericServer to GenServer from FourOhFourCounter

--- lib/servy/404_counter.ex
+++ lib/servy/404_counter.ex
@@ -1,37 +1,42 @@
 defmodule Servy.FourOhFourCounter do
+  use GenServer
+
   @name __MODULE__
 
-  alias Servy.GenericServer
-
   def start(initial_state \\ %{}) do
-    GenericServer.start(__MODULE__, @name, initial_state)
+    GenServer.start(__MODULE__, initial_state, name: @name)
   end
 
   def bump_count(path) do
-    GenericServer.call(@name, {:bump_count, path})
+    GenServer.call(@name, {:bump_count, path})
   end
 
   def get_counts do
-    GenericServer.call(@name, :get_counts)
+    GenServer.call(@name, :get_counts)
   end
 
   def get_count(path) do
-    GenericServer.call(@name, {:get_count, path})
+    GenServer.call(@name, {:get_count, path})
   end
 
   # SERVER CALLBACKS
-  def handle_call({:bump_count, path}, state) do
+
+  def init(init_args) do
+    {:ok, init_args}
+  end
+
+  def handle_call({:bump_count, path}, _from, state) do
     new_state = Map.update(state, path, 1, &(&1 + 1))
 
-    {:ok, new_state}
+    {:reply, :ok, new_state}
   end
 
-  def handle_call(:get_counts, state) do
-    {state, state}
+  def handle_call(:get_counts, _from, state) do
+    {:reply, state, state}
   end
 
-  def handle_call({:get_count, path}, state) do
+  def handle_call({:get_count, path}, _from, state) do
     count = Map.get(state, path, 0)
-    {count, state}
+    {:reply, count, state}
   end
 end
