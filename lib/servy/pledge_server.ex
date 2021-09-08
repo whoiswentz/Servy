2c5227d4469e9cdff907e8629b9897cb2349b11f
Vinicios Wentz <vinicios@wentz.io>
2021-09-07 22:43:56 -0300

    feat: implementing a 'GenServer' by hand

--- lib/servy/pledge_server.ex
+++ lib/servy/pledge_server.ex
@@ -1,68 +1,88 @@
+defmodule Servy.GenericServer do
+  def start(callback_module, name, initial_state \\ []) do
+    pid = spawn(__MODULE__, :listen_loop, [initial_state, callback_module])
+    Process.register(pid, name)
+    pid
+  end
+
+  def call(pid, message) do
+    send(pid, {:call, self(), message})
+
+    receive do
+      {:response, respose} ->
+        respose
+    end
+  end
+
+  def cast(pid, message) do
+    send(pid, {:cast, message})
+  end
+
+  def listen_loop(state, callback_module) do
+    receive do
+      {:call, sender, message} ->
+        {response, new_state} = callback_module.handle_call(message, state)
+        send(sender, {:response, response})
+        listen_loop(new_state, callback_module)
+
+      {:cast, message} ->
+        new_state = callback_module.handle_cast(message, state)
+        listen_loop(new_state, callback_module)
+
+      _ ->
+        listen_loop(state, callback_module)
+    end
+  end
+end
+
 defmodule Servy.PledgeServer do
   @process_name __MODULE__
 
+  alias Servy.GenericServer
+
   def start(initial_state \\ []) do
-    pid = spawn(__MODULE__, :listen_loop, [initial_state])
-    Process.register(pid, @process_name)
-    pid
+    GenericServer.start(__MODULE__, @process_name)
   end
 
   # CLIENT INTERFACES
 
   def create_pledge(name, amount) do
-    send(@process_name, {self(), :create_pledge, name, amount})
-
-    receive do
-      {:response, id} ->
-        id
-    end
+    GenericServer.call(@process_name, {:create_pledge, name, amount})
   end
 
   def recent_pledge do
-    send(@process_name, {self(), :recent_pledges})
-
-    receive do
-      {:response, pledges} ->
-        pledges
-    end
+    GenericServer.call(@process_name, :recent_pledges)
   end
 
   def total_pledged do
-    send(@process_name, {self(), :total_pledged})
-
-    receive do
-      {:response, total} ->
-        total
-    end
+    GenericServer.call(@process_name, :total_pledged)
   end
 
-  # SERVER INTERFACES
+  def clear do
+    GenericServer.cast(@process_name, :clear)
+  end
 
-  def listen_loop(state) do
-    receive do
-      {sender, :create_pledge, name, amount} ->
-        {:ok, id} = send_pledge_to_service(name, amount)
-        most_recent_pledges = Enum.take(state, 2)
-        new_state = [{name, amount} | most_recent_pledges]
-        send(sender, {:response, id})
-        listen_loop(new_state)
+  def handle_call(:total_pledged, state) do
+    total = Enum.reduce(state, 0, fn {_name, amount}, acc -> amount + acc end)
+    {total, state}
+  end
 
-      {sender, :recent_pledges} ->
-        send(sender, {:response, state})
-        listen_loop(state)
+  def handle_call({:create_pledge, name, amount}, state) do
+    {:ok, id} = send_pledge_to_service(name, amount)
+    most_recent_pledges = Enum.take(state, 2)
+    new_state = [{name, amount} | most_recent_pledges]
+    {id, new_state}
+  end
 
-      {sender, :total_pledged} ->
-        total = Enum.reduce(state, 0, fn {_name, amount}, acc -> amount + acc end)
-        send(sender, {:response, total})
-        listen_loop(state)
+  def handle_call(:recent_pledges, state) do
+    {state, state}
+  end
 
-      _ ->
-        listen_loop(state)
-    end
+  def handle_cast(:clear, _state) do
+    []
   end
 
   # PRIVATE FUNCTIONS
-
   defp send_pledge_to_service(_name, _amount) do
     {:ok, "pledge-#{:rand.uniform(1000)}"}
   end
