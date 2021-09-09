0dd4df41c3d4849d0c76d50f2c9783d544871970
Vinicios Wentz <vinicios@wentz.io>
2021-09-08 22:13:30 -0300

    feat: linking http process to a GenServer to handler failures

---
+++ lib/servy/kickstarter_server.ex
@@ -0,0 +1,25 @@
+defmodule Servy.KickstarterServer do
+  use GenServer
+
+  def start do
+    GenServer.start(__MODULE__, :ok, name: __MODULE__)
+  end
+
+  defp start_server do
+    server_pid = spawn_link(Servy, :start, [4000])
+    Process.register(server_pid, :http_server)
+    server_pid
+  end
+
+  def init(:ok) do
+    Process.flag(:trap_exit, true)
+    server_pid = start_server()
+
+    {:ok, server_pid}
+  end
+
+  def handle_info({:EXIT, _pid, _reason}, _state) do
+    server_pid = start_server()
+    {:noreply, server_pid}
+  end
+end
